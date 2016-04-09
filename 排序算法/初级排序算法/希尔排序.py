2a677a3df4da5221825cac91cfc4c2ee481f3ea0
mayne <dreama9ain@gmail.com>
2016-04-09 18:47:04 +0800

    done

--- 排序算法/初级排序算法/希尔排序.py
+++ 排序算法/初级排序算法/希尔排序.py
@@ -4,6 +4,9 @@
 已知的最好步长序列是由Sedgewick提出的(1, 5, 19, 41, 109,...)
 参见:https://zh.wikipedia.org/wiki/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
 '''
+import math
+
+#插入排序
 def insertion_sort(a):
     if len(a)==1:
         return a
@@ -17,4 +20,34 @@ def insertion_sort(a):
             i-=1
     return a
 
+#得到步长序列
+def getStepnum(num):
+    list=[]
+    for i in range(num):
+        list.append(9*4**i-9*2**i+1)
+        list.append(2**(i+2)*(2**(i+2)-3)+1)
+    list.sort()
+    list.reverse()
+    return list
+
+#希尔排序
 def shell_sort(a):
+    m = len(a)
+    b=[]
+    i=1
+    while m>getStepnum(i)[0]:
+        i+=1
+    step = getStepnum(i-1)
+    print(step)
+    for i in step:
+        if i>1:
+            for j in range(i):
+                for k in range(math.ceil(m/i)):
+                    if (j+k*i)<m:
+                        b.append(a[j+k*i])
+                print(insertion_sort(b[j*i:(j+1)*i]))
+        else:
+            insertion_sort(b)
+    return b
+print(shell_sort([1,2,3,123,12,41,24,12,21,4,12,412,53,25,3,53,25,252,3,5,23,5,23,5,235,4,6,5,9,7,8,0]))
+
